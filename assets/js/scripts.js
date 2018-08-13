de4cbe627b41ffaae63363af512a647640f24a1b
nivred <emaildervin@gmail.com>
2018-08-13 14:56:06 -0500

    added js code with variables for calculate function

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+document.querySelector('#loan-form').addEventListener('submit', calculateResults);
+
+// CalculateResults
+function calculateResults(e) {
+    console.log('Calculating...')
+
+    e.preventDefault();
+}
