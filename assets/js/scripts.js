d01185465fe80b71560f2e510500d6104c45e6ed
nivred <emaildervin@gmail.com>
2018-08-13 15:51:39 -0500

    added bootstrap styling to results output

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,6 +3,29 @@ document.querySelector('#loan-form').addEventListener('submit', calculateResults
 // CalculateResults
 function calculateResults(e) {
     console.log('Calculating...')
+    // UI Variables
+    const amount = document.querySelector('#amount');
+    const interest = document.querySelector('#interest');
+    const years = document.querySelector('#years');
+    const monthlyPayment = document.querySelector('#monthly-payment');
+    const totalPayment = document.querySelector('#total-payment');
+    const totalInterest = document.querySelector('#total-interest');
+
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+    
+    // Compute monthly payment
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest) / (x -1);
+
+    if(isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    } else {
+        console.log('Error. Please check inputted values')
+    }
 
     e.preventDefault();
 }
